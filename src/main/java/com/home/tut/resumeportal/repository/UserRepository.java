260eb5ef81ccf55663e24a471e3623124e277222
Zoheb Syed <zoheb.syed@outlook.com>
2021-07-19 00:16:54 +0530

    Base Setup Project, Define User Entity configure it to use Spring Security 	User Service

---
+++ src/main/java/com/home/tut/resumeportal/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.home.tut.resumeportal.repository;
+
+import com.home.tut.resumeportal.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByUserName(String userName);
+}
